e3273cde5264455a485fcaa65dcca1501db46b56
colesayer <colesayershapiro@gmail.com>
2017-11-21 16:35:56 -0500

    Done.

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -5,7 +5,7 @@ export class Restaurants extends Component {
 
   render() {
 
-    let restaurants = this.props.restaurants.map((restaurant, index) => <li key={index}>{restaurant.name}</li>);
+    let restaurants = this.props.restaurants.map((restaurant, index) => <li key={index}><strong>{restaurant.name}:</strong> {restaurant.location}</li>);
 
     return (
       <div>
